94ab9cd287b6f452fd8a07695a126b5297262138
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-09-22 23:12:51 +0300

    Add validators for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true, length: { in: 3..20 }
+  validates :email, uniqueness: true, presence: true, format: URI::MailTo::EMAIL_REGEXP
+  validates :password, presence: true, length: { minimum: 8 }
 end
